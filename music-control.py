52cbc4435434f35f918391c438065ea99045ba28
Alex Couture-Beil <acouturebeil@digitalocean.com>
2016-06-16 19:16:29 -0700

    restart stream when mpc is not playing anything

--- music-control.py
+++ music-control.py
@@ -11,14 +11,22 @@ GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 radio = {
     'kexp': 'http://216.246.37.218:80',
     'witr': 'http://streaming.witr.rit.edu:8000/witr-undrgnd-mp3-192.m3u',
-#    'dfm':  'http://176.9.138.206:8205/dfm_1',
     'kcrw': 'http://newmedia.kcrw.com/legacy/pls/kcrwmusic.pls',
+#    'dfm':  'http://176.9.138.206:8205/dfm_1',
 #    'irish': 'http://www.irishpubradio.com/streams1/listen.pls',
     }
 
 radio_iter = cycle(radio.values())
 
 state = None
+
+def is_playing():
+    try:
+        x = subprocess.check_output(['/usr/bin/mpc', 'current']).trim()
+    except:
+        return False
+    return bool(x)
+
 
 def start_playing():
     url = radio_iter.next()
@@ -42,4 +50,6 @@ while True:
         if state == None or state == 'on':
             stop_playing()
             state = 'off'
+    if not is_playing():
+        state = None
 
