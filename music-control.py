94147afd32a7596aed0bb9e3d9853bc4c44ae0c3
Alex Couture-Beil <acouturebeil@digitalocean.com>
2016-06-16 20:08:06 -0700

    more tweaks

--- music-control.py
+++ music-control.py
@@ -2,6 +2,7 @@
 import RPi.GPIO as GPIO
 import time
 import subprocess
+import time
 from itertools import cycle
 
 GPIO.setmode(GPIO.BCM)
@@ -11,8 +12,8 @@ GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 radio = {
     'kexp': 'http://216.246.37.218:80',
     'witr': 'http://streaming.witr.rit.edu:8000/witr-undrgnd-mp3-192.m3u',
-    'kcrw': 'http://newmedia.kcrw.com/legacy/pls/kcrwmusic.pls',
 #    'dfm':  'http://176.9.138.206:8205/dfm_1',
+    'kcrw': 'http://newmedia.kcrw.com/legacy/pls/kcrwmusic.pls',
 #    'irish': 'http://www.irishpubradio.com/streams1/listen.pls',
     }
 
@@ -20,24 +21,27 @@ radio_iter = cycle(radio.values())
 
 state = None
 
+next_try_is_playing = 0
 def is_playing():
     try:
-        x = subprocess.check_output(['/usr/bin/mpc', 'current']).trim()
-    except:
+        x = subprocess.check_output(['/usr/bin/mpc', 'current']).strip()
+    except Exception as e:
         return False
     return bool(x)
 
+def set_volume():
+    subprocess.call(['/usr/bin/mpc', 'volume', '100'])
 
 def start_playing():
+    if is_playing():
+        return
     url = radio_iter.next()
     subprocess.call(['/usr/bin/mpc', 'clear'])
     subprocess.call(['/usr/bin/mpc', 'add', url])
     subprocess.call(['/usr/bin/mpc', 'play'])
-    print 'START'
 
 def stop_playing():
     subprocess.call(['/usr/bin/mpc', 'stop'])
-    print 'STOP'
 
 while True:
     input_state = GPIO.input(18)
@@ -50,6 +54,9 @@ while True:
         if state == None or state == 'on':
             stop_playing()
             state = 'off'
-    if not is_playing():
-        state = None
+    if next_try_is_playing < time.time():
+        next_try_is_playing = time.time() + 30
+        set_volume()
+        if not is_playing():
+            state = None
 
